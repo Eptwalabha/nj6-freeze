fc0a3e60f82dd64d6b3c8f307f58af06ab671428
damien <damiengabrielle@gmail.com>
2024-02-21 21:40:22 +0100

    player animation + animation tree

--- scene/core/player.gd
+++ scene/core/player.gd
@@ -1,8 +1,7 @@
 class_name Player
 extends CharacterBody2D
 
-const SPEED = 30.0
-const JUMP_VELOCITY = -40.0
+const SPEED = 15.0
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
@@ -11,13 +10,20 @@ const JUMP_VELOCITY = -40.0
 var heating : bool = false
 var working : bool = false
 
+func _ready() -> void:
+	animation_tree.active = true
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += 50 * delta
-	velocity.x = Input.get_axis("move_left", "move_right") * 42.0
+	velocity.x = Input.get_axis("move_left", "move_right") * SPEED
 	move_and_slide()
 	_update_player_states()
 
 func _update_player_states() -> void:
+	var moving = false
 	if velocity.x != 0:
+		moving = true
 		sprite_2d.flip_h = velocity.x < 0
+	animation_tree.set("parameters/conditions/moving", moving)
+	animation_tree.set("parameters/conditions/idle", not moving)
