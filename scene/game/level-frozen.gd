ff664a12fd158e8cb8f847eef7382d52a34e5853
damien <damiengabrielle@gmail.com>
2024-02-24 03:45:50 +0100

    add dialog box

--- scene/game/level-frozen.gd
+++ scene/game/level-frozen.gd
@@ -2,13 +2,33 @@ extends Node
 
 @onready var ui_animation_player: AnimationPlayer = $UI/UIAnimationPlayer
 @onready var phone: PhoneUI = $UI/Nokia
+@onready var dialog_ui: DialogUI = $UI/DialogUI
 
 var phone_visible : bool = false
+var dialog_visible : bool = true
+
+func _ready() -> void:
+	dialog_ui.set_dialog_lines([
+		"Welcome to the 6th annual Nokia 3310 Jam! Work solo or make a team to create a game using the restrictions of the original Nokia 3310 phone from the 2000s!",
+		"Below you will find the rules, restrictions, and ranking details of the jam. Have fun working within these restrictions! Games that fail to follow these rules will be disqualified from being ranked. For more detailed questions please view the FAQ in the jam's community tab."])
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("action-confirm"):
-		if phone_visible:
-			ui_animation_player.play("phone-hide")
+		if dialog_visible:
+			dialog_ui.next_dialog()
 		else:
-			ui_animation_player.play("phone-show")
-		phone_visible = !phone_visible
+			toggle_phone_visibility(!phone_visible)
+
+func _on_dialog_ui_dialog_ended() -> void:
+	dialog_ui.visible = false
+	dialog_visible = false
+
+func _on_dialog_ui_new_line_ended(line_index: Variant) -> void:
+	toggle_phone_visibility(line_index >= 1)
+
+func toggle_phone_visibility(is_visible: bool) -> void:
+	if is_visible and not phone_visible:
+		ui_animation_player.play("phone-show")
+	elif not is_visible and phone_visible:
+		ui_animation_player.play("phone-hide")
+	phone_visible = is_visible
---
+++ scene/ui/dialog.gd
@@ -0,0 +1,115 @@
+class_name DialogUI
+extends Node2D
+
+signal dialog_started
+signal dialog_ended
+signal new_line_started(line_index)
+signal new_line_ended(line_index)
+
+@onready var next_arrow: Sprite2D = $NinePatchRect/NextArrow
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var dialog: Label = %Dialog
+
+@export var nbr_letters_per_second : int = 40 : set = _set_speed
+
+enum DIALOG_STATE {
+	HIDDEN,
+	TYPING,
+	WAITING,
+	DONE,
+}
+
+var current_line_index : int = 0
+var lines : Array[String] = []
+var nbr_lines : int = 0
+var char_timer : float = 0.1
+var t : float = 0
+var state : DIALOG_STATE = DIALOG_STATE.HIDDEN;
+
+const MAX_CHARACTERS : int = 34
+const MAX_CHARACTERS_PER_LINE : int = 17
+
+func set_dialog_lines(new_lines: Array[String], _options: Dictionary = {}) -> void:
+	lines = []
+	for line in new_lines:
+		line = tr(line)
+		if len(line) > MAX_CHARACTERS:
+			lines.append_array(_split_line(line))
+		else:
+			lines.append(line)
+	nbr_lines = len(lines)
+	current_line_index = -1
+
+func _split_line(line: String) -> Array[String]:
+	var lines_acc : Array[String] = []
+	var current_line : String = ""
+	var current_word : String = ""
+	for letter in line:
+		current_word += letter
+		if letter == " ":
+			if len(current_line) > MAX_CHARACTERS_PER_LINE:
+				lines_acc.append(current_line)
+				current_line = ""
+			current_line += current_word
+			current_word = ""
+		else:
+			if len(current_line + current_word) > MAX_CHARACTERS_PER_LINE:
+				lines_acc.append(current_line)
+				current_line = ""
+	current_line += current_word
+	if len(current_line) > 0:
+		lines_acc.append(current_line)
+	var final_lines : Array[String] = []
+	var line_count : int = 0
+	current_line = ""
+	for line_acc in lines_acc:
+		current_line += line_acc
+		line_count += 1
+		if line_count > 1:
+			line_count = 0
+			final_lines.append(current_line)
+			current_line = ""
+	return final_lines
+
+func _process(delta: float) -> void:
+	if state == DIALOG_STATE.TYPING:
+		t -= delta
+		if t < 0:
+			t += char_timer
+			dialog.visible_characters += 1
+			if dialog.visible_ratio >= 1.0:
+				_end_of_line()
+
+func next_dialog() -> void:
+	if nbr_lines == 0:
+		return
+	if state == DIALOG_STATE.DONE:
+		dialog_ended.emit()
+	if state == DIALOG_STATE.TYPING:
+		dialog.visible_characters = -1
+		_end_of_line()
+	elif state == DIALOG_STATE.DONE:
+		visible = false
+		state = DIALOG_STATE.HIDDEN
+	else:
+		if state == DIALOG_STATE.HIDDEN:
+			dialog_started.emit()
+		current_line_index += 1
+		dialog.visible_characters = 0
+		dialog.text = tr(lines[current_line_index])
+		new_line_started.emit(current_line_index)
+		state = DIALOG_STATE.TYPING
+		next_arrow.visible = false
+
+func _end_of_line() -> void:
+	if current_line_index < (nbr_lines - 1):
+		state = DIALOG_STATE.WAITING
+		next_arrow.visible = true
+	else:
+		state = DIALOG_STATE.DONE
+		next_arrow.visible = false
+	new_line_ended.emit(current_line_index)
+
+func _set_speed(new_nbr_letters_per_second: int) -> void:
+	nbr_letters_per_second = new_nbr_letters_per_second
+	char_timer = 1.0 / float(nbr_letters_per_second)
