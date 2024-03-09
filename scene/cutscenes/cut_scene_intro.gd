e98d73aee48ed561f0d16c0c7bc9c66f9f0ef9c5
damien <damiengabrielle@gmail.com>
2024-03-09 23:37:00 +0100

    #13 wip cut scene

---
+++ scene/cutscenes/cut_scene_intro.gd
@@ -0,0 +1,11 @@
+class_name CutSceneIntro
+extends CutScene
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func start() -> void:
+	super.start()
+	animation_player.play("intro")
+
+func wake_up_player() -> void:
+	pass
---
+++ scene/cutscenes/in-game/cut_scene.gd
@@ -0,0 +1,22 @@
+class_name CutScene
+extends Node
+
+signal cut_scene_stated(cut_scene_id)
+signal cut_scene_ended(cut_scene_id)
+
+var level : GameLevel
+
+@export var cut_scene_id : StringName = ""
+
+func _ready() -> void:
+	if cut_scene_id == "":
+		cut_scene_id = name
+	level = owner
+
+
+func start() -> void:
+	cut_scene_stated.emit(cut_scene_id)
+
+
+func end() -> void:
+	cut_scene_ended.emit(cut_scene_id)
