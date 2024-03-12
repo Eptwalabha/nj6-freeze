fc2d408ed899a8aa21b444e0a30009bae3a46c7e
damien <damiengabrielle@gmail.com>
2024-03-12 01:09:50 +0100

    #13 fix gdformat

--- scene/cutscenes/cut_scene_intro.gd
+++ scene/cutscenes/cut_scene_intro.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 	super()
 	GameData.triggered.connect(_on_triggered_emited)
 
+
 func start() -> void:
 	level.reset_player_position()
 	level.player.enters_cutscene(true)
@@ -19,8 +20,9 @@ func start() -> void:
 	await GameData.dialog_ended
 	level.player.enters_cutscene(false)
 
+
 func _on_triggered_emited(trigger_id: StringName) -> void:
-	if trigger_id == "car_battery_died":
+	if trigger_id == &"car_battery_died":
 		level.player.enters_cutscene(true)
 		GameUI.request_dialog($Dialogs/CarBatteryIsDead)
 		await GameData.dialog_ended
@@ -37,11 +39,12 @@ func battery_car_dies_out() -> void:
 	shade.global_position = level.player.global_position + Vector2(60.0, 0.0)
 	shade.target = level.player
 	shade.activate()
-	shade.escaped.connect(
-		func () -> void:
-			GameUI.request_dialog($Dialogs/FirstEncounter)
-			await GameData.dialog_ended
-			level.player.enters_cutscene(false)
-	)
+	shade.escaped.connect(_on_first_shade_escaped)
 	await get_tree().create_timer(2.2).timeout
 	GameData.flashlight_found.emit()
+
+
+func _on_first_shade_escaped() -> void:
+	GameUI.request_dialog($Dialogs/FirstEncounter)
+	await GameData.dialog_ended
+	level.player.enters_cutscene(false)
